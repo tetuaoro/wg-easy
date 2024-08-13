32ccf79cfc9cdf1be7b35043d89611c049b335d7
Bernd Storath <999999bst@gmail.com>
2024-08-13 11:00:23 +0200

    shutdown wireguard properly

--- src/middleware/auth.global.ts
+++ src/middleware/auth.global.ts
@@ -1,7 +1,10 @@
 export default defineNuxtRouteMiddleware(async (to) => {
-  // TODO: cache session
+  // TODO?: cache session
   const session = await useUISession();
   if (!session) {
+    if (to.path !== 'login') {
+      return navigateTo('/login');
+    }
     return abortNavigation();
   }
   if (to.path === '/login') {
